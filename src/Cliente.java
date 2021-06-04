faf2b47461bb2171dac6f24b6a078e986d297e14
joao-21905131 <joaopedro1678@hotmail.com>
2021-06-04 10:21:31 +0100

    So falta testar

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,14 +2,23 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-
 public class Cliente extends Thread {
     private ServerSocket serverSocket;
+    private DatagramSocket socket;
+    private InetAddress address;
+    private byte[] pos = new byte[256];
+    int posicaoLista=0;
+    private static String ip;
+    private DatagramSocket com;
+
+    public Cliente() {
+    }
 
     public Cliente(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         serverSocket.setSoTimeout(10000);
     }
+
     private static final String menu() {
         return "MENU CLIENTE\n" +
                 "0 - Menu Inicial\n"+
@@ -20,38 +29,80 @@ public class Cliente extends Thread {
                 "5 - lista negra de utilizadores\n"+
                 "99 – Sair\n";
     }
-    public void run(){
-        try{
-            menu();
-            Scanner opcaoMenu = new Scanner(System.in);
-            int userName = opcaoMenu.nextInt();
 
+    public static Scanner lido;
+
+    public void run()
+    {
+        Socket socket;
+
+        try {
+            socket = new Socket(ip, 7142);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+
+            int userName = 0;
             do {
+                System.out.println(menu());
+                System.out.println("Indique o comando que queira executar: ");
+                Scanner opcaoMenu = new Scanner(System.in);
+                userName = opcaoMenu.nextInt();
                 switch (userName){
                     case 0:
                         //Apresenta o menu novamente
-                        menu();
+                        ps.println("0");
+                        String texto = input.readLine();
+                        while (!texto.equals("")){
+                            System.out.println(input.readLine());
+                            texto = input.readLine();
+                        }
                         break;
                     case 1:
+                        ps.println("1");
                         System.out.println("Utilizadores onlines:\n");
                         break;
                     case 2:
-                        StringBuilder answer = new StringBuilder();
-                        //for (host: Servidor)
+                        ps.println("2");
+                        try {
+                            input.readLine();
+                            String mensagemIP = lido.nextLine();
+                            ps.println(mensagemIP);
+                            input.readLine();
+                            String mensagem = lido.nextLine();
+                            ps.println(mensagem);
+                            this.address = InetAddress.getByName(ip);
+                            pos = mensagem.getBytes();
+                            DatagramPacket packet = new DatagramPacket(pos, pos.length, address, 9031);
+                            com.send(packet);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         break;
                     case 3:
+                        ps.println("3");
+                        String mensagem = lido.nextLine();
+                        ps.println(mensagem);
+                        com = new DatagramSocket();
+                        this.address = InetAddress.getByName(ip);
+                        pos = mensagem.getBytes();
+                        DatagramPacket packet = new DatagramPacket(pos, pos.length, address, 9031);
+                        com.send(packet);
                         break;
+
                     case 4:
+                        ps.println("4");
                         System.out.println("Lista branca:\n");
                         System.out.println(Servidor.listaBranca);
                         break;
                     case 5:
+                        ps.println("5");
                         System.out.println("Lista negra:\n");
                         //System.out.println(listaNegra);
                         break;
                     case 99:
                         System.out.println("A Sair\n");
                         System.out.println("Cliente Desconhectado..");
+                        socket.close();
                         break;
                 }
             }while (userName != 99);
@@ -86,13 +137,22 @@ public class Cliente extends Thread {
 
          */
     }
-
-
-
     // usage: java EchoClient <servidor> <mensagem>
-    public static void main(String args[]) throws Exception {
-        //Apresenta o menu
+    public static void main(String args[]) {
+        Cliente novo = new Cliente();
+        novo.start();
+        ip = args[0];
+
+
+
+       /*
+       lido= new Scanner(System.in);
+
+
+
+       //Apresenta o menu
         menu();
+
         //Leitura da opção
         Scanner opcaoMenu = new Scanner(System.in);
         int userName = opcaoMenu.nextInt();
@@ -109,6 +169,13 @@ public class Cliente extends Thread {
         System.out.println("Recebido : " + br.readLine());
 // termina socket
         socket.close();
+
+
+
+        teclado = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+        cliente.start();
+        */
     }
 
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,10 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -13,9 +8,23 @@ public class Servidor {
     static HashMap<String, String> listaNegra = new HashMap();
     static HashMap<String, String> listaBranca = new HashMap();
     static HashMap<String, String> listaOnline = new HashMap();
+    static BufferedReader leitura;
+    static PrintStream printStream;
 
     public static void leituraListas()  throws FileNotFoundException {
+        //Forma errada, tem que estar num ficheiro txt
+        listaNegra= new HashMap<>();
+        listaNegra.put("192.168.10.10","OFFLINE");
+        listaNegra.put("192.168.10.11","OFFLINE");
+        listaNegra.put("192.168.20.17","OFFLINE");
 
+
+        listaNegra = new HashMap<>();
+        listaNegra.put("192.168.10.07","OFFLINE");
+        listaNegra.put("192.168.0.09","OFFLINE");
+        listaNegra.put("127.0.0.1","OFFLINE");
+
+        /*
         Scanner leitorFicheiroBranca = new Scanner(new FileReader("listaBranca.txt"));
         System.out.println("2");
         while (leitorFicheiroBranca.hasNext()) {
@@ -40,31 +49,147 @@ public class Servidor {
                 String estado = dados[1].trim();
                 System.out.println(estado);
                 //listaNegra.put(ip, estado);
-
             }
         }
         //System.out.println(listaBranca);
         System.out.println("lista");
         //System.out.println(listaNegra);
+ */
+    }
+
+     static class Server_Manager implements Runnable {
+        private  final String menu() {
+            return "MENU CLIENTE\n" +
+                    "0 - Menu Inicial\n" +
+                    "1 - Listar utilizadores online\n" +
+                    "2 - Enviar mensagem a um utilizador\n" +
+                    "3 - Enviar mensagem a todos os utilizadores\n" +
+                    "4 - lista branca de utilizadores\n" +
+                    "5 - lista negra de utilizadores\n" +
+                    "99 – Sair\n";
+        }
+        public Server_Manager (Socket s) throws IOException{
+             this.socket = s;
+             //s.start()
+             leitura= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             printStream = new PrintStream(socket.getOutputStream());
+         }
+        private DatagramSocket com;
+        private byte[] pos = new byte[256];
+        private Socket socket;
+        int posicaoLista = 0;
+        private InetAddress address;
+        private byte[] buf = new byte[256];
+        //envio
+
+        public void run() {
+
+            try {
+                String userName = leitura.readLine();
+                do {
+                    switch (userName) {
+                        case "0":
+                            //Apresenta o menu novamente
+                            System.out.println("0");
+                            printStream.println(menu());
+                            break;
+                        case "1": {
+                            System.out.println("Utilizadores onlines:\n");
+                            System.out.println(listaOnline.toString());
+                            break;
+                        }
+                        case "2": {
+                            System.out.println("Qual é o IP que deseja enviar uma mensagem? ");
+                            String mensagemIP = leitura.readLine();
+                            StringBuilder answer = new StringBuilder();
+                            System.out.println("Qual é a mensagem que pretende enviar? ");
+                            String mensage = leitura.readLine();
+                            byte[] mensagemBytes = mensage.getBytes();
+                            com = new DatagramSocket();
+
+                            try {
+                                this.address = InetAddress.getByName(mensagemIP);
+                                DatagramPacket packet = new DatagramPacket(mensagemBytes, mensagemBytes.length, address, 9031);
+                                com.send(packet);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            com.close();
+                            //printStream a fechar??
+                            break;
+                        }
+                        case "3": {
+                            System.out.println("Qual é a mensagem que pretende enviar? ");
+                            String mens = leitura.readLine();
+                            byte[] mensagemBytes = mens.getBytes();
+                            com = new DatagramSocket();
+
+                            for (String i : listaOnline.keySet()) {
+                                try {
+                                    this.address = InetAddress.getByName(i);
+                                    DatagramPacket packet = new DatagramPacket(mensagemBytes, mensagemBytes.length, address, 9031);
+                                    com.send(packet);
+                                } catch (IOException error) {
+                                    error.printStackTrace();
+                                }
+                            }
+
+                            com.close();
+                            break;
+                        }
+                        case "4": {
+                            System.out.println("Lista branca:\n");
+                            System.out.println(listaBranca);
+                            break;
+                        }
+                        case "5": {
+                            System.out.println("Lista negra:\n");
+                            System.out.println(listaNegra);
+                            break;
+                        }
+                        case "99": {
+                            System.out.println("A Sair\n");
+                            System.out.println("Cliente Desconhectado..");
+                            break;
+                        }
+                    }
+                } while (!userName.equals("99"));
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
+            com.close();
+        }
+
     }
 
     public static void main(String args[]) throws Exception {
-
-
-        leituraListas();
-        //Criar lista online, e a medida que as pessoas vao entrando, ele vao conectares, tem que ler de um ficheiro e selecionar o que sao da lista negra e da lista branca
-        /*
         listaNegra.put("192.168.10.10", "Offline");
         listaNegra.put("192.168.10.15", "Offline");
         listaNegra.put("192.168.10.110", "Offline");
 
-        listaBranca.put("192.168.10.12", "Online");
-        listaBranca.put("192.168.10.22", "Online");
-        listaBranca.put("192.168.10.110", "Online");
-        listaBranca.put("192.168.10.74", "Online");
-        */
+        listaBranca.put("192.168.10.12", "Offline");
+        listaBranca.put("192.168.10.22", "Offline");
+        listaBranca.put("192.168.10.110", "Offline");
+        listaBranca.put("192.168.10.74", "Offline");
+
+        listaOnline.put("192.168.10.191","Online");
+        ServerSocket server = new ServerSocket(7142);
+        Socket socket = null;
+        while (true){
+            socket = server.accept();
+            PrintStream stream = new PrintStream(socket.getOutputStream());
+            stream.println(" ");
+            Thread thread = new Thread(new Server_Manager(socket));
+            thread.start();
+        }
 
 
+
+
+        // leituraListas();
+        //Criar lista online, e a medida que as pessoas vao entrando, ele vao conectares, tem que ler de um ficheiro e selecionar o que sao da lista negra e da lista branca
+        /*
 /*
         ServerSocket server = new ServerSocket(6500);
         Socket socket= null;
@@ -83,60 +208,6 @@ public class Servidor {
 
 
 
-    }
-
-    /* Notas:
-    - Lista Negra tem prioridade
-
-    */
-    private static final String menu() {
-        return "MENU CLIENTE\n" +
-                "0 - Menu Inicial\n" +
-                "1 - Listar utilizadores online\n" +
-                "2 - Enviar mensagem a um utilizador\n" +
-                "3 - Enviar mensagem a todos os utilizadores\n" +
-                "4 - lista branca de utilizadores\n" +
-                "5 - lista negra de utilizadores\n" +
-                "99 – Sair\n";
-    }
-
-    class ClienteServidor implements Runnable {
-
-        public void run() {
-            menu();
-            Scanner opcaoMenu = new Scanner(System.in);
-            int userName = opcaoMenu.nextInt();
-
-            do {
-                switch (userName) {
-                    case 0:
-                        //Apresenta o menu novamente
-                        menu();
-                        break;
-                    case 1:
-                        System.out.println("Utilizadores onlines:\n");
-                        break;
-                    case 2:
-                        StringBuilder answer = new StringBuilder();
-
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        System.out.println("Lista branca:\n");
-                        System.out.println(listaBranca);
-                        break;
-                    case 5:
-                        System.out.println("Lista negra:\n");
-                        System.out.println(listaNegra);
-                        break;
-                    case 99:
-                        System.out.println("A Sair\n");
-                        System.out.println("Cliente Desconhectado..");
-                        break;
-                }
-            } while (userName != 99);
-        }
     }
 }
 
